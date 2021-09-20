59d8429abd8a0d48f4ab9f11e9d1dbb8efede72d
Pavan Rathod <edroculllen13@gmail.com>
2021-09-20 13:49:06 +0530

    Adding More images and updates

--- src/components/Story.js
+++ src/components/Story.js
@@ -3,23 +3,33 @@ const Story = () => {
     <section className="py-16 px-3">
       <div>
         <h1 className="text-center text-5xl sm:text-7xl lg:text-9xl font-serif">
-          Trending in Dark <span className="text-red-700">Mountain</span>
+          History of Dark <span className="text-red-700">Mountain</span>
         </h1>
       </div>
 
       <div className="lg:grid lg:grid-cols-10 lg:gap-5 py-10 items-center">
-        <div className="story__one col-span-6 opacity-90 hover:opacity-100 transition duration-500 ease-in-out">
-          <div className="">
-            <div>
-              <h1>
-                <h1>THIS IS BEEER</h1>
+        <div className="story__one col-span-6 opacity-90 hover:opacity-100 transition duration-500 ease-in-out relative">
+          <div className="absolute bottom-0">
+            <h1 className="bg-red-700 py-4 px-4 text-white font-mono">
+              Amsterdam 1968 with Robert Pattinson and Alferd Molina
+            </h1>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-4 lg:col-start-7 lg:col-span-full lg:grid-rows-2 pt-4 lg:pt-0">
+          <div className="story__two lg:col-span-full opacity-90 hover:opacity-100 transition duration-500 ease-in-out relative">
+            <div className="absolute bottom-0 right-0 ">
+              <h1 className="bg-red-700 py-4 px-4 text-white font-mono">
+                1970 New York Our first Barrel dis
               </h1>
             </div>
           </div>
-        </div>
-        <div className="grid grid-cols-2 gap-4 lg:col-start-7 lg:col-span-full lg:grid-rows-2">
-          <div className="story__two lg:col-span-full opacity-90 hover:opacity-100 transition duration-500 ease-in-out"></div>
-          <div className="story__three lg:col-span-full opacity-90 hover:opacity-100 transition duration-500 ease-in-out"></div>
+          <div className="story__three lg:col-span-full opacity-90 hover:opacity-100 transition duration-500 ease-in-out relative">
+            <div className="absolute bottom-0 right-0">
+              <h1 className="bg-red-700 py-4 px-4 text-white font-mono">
+                Happy customers in West side Story
+              </h1>
+            </div>
+          </div>
         </div>
       </div>
     </section>
